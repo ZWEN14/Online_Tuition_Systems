834387c4946c27a7eb2829069f27b9e2fa2193e9
zw <lzwen05@gmail.com>
2026-08-29 23:32:44 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Online_Tuition_Systems.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
